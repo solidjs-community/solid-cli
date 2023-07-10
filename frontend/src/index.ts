288495f8c9d8e7613ab1a3511cf8293e1612d61c
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-07-10 21:19:27 +0100

    Rewritten using cmd-ts for command line argument parsing

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,87 +1,55 @@
+import {
+	binary,
+	command,
+	oneOf,
+	positional,
+	run,
+	string,
+	subcommands,
+} from "cmd-ts";
 import * as p from "@clack/prompts";
 import color from "picocolors";
-import { transform } from "@swc/core";
-import { readFile, writeFile } from "fs/promises";
-import { exec } from "child_process";
+import {
+	ConfigTransform,
+	resolvePluginConfig,
+	supported,
+} from "./lib/ConfigTransform";
 import { detect } from "detect-package-manager";
-// [importName, importSource, isDefaultImport]
-type ResolvePluginConfigRet = [string, string, boolean];
-const resolvePluginConfig = (
-	packageName: string
-): ResolvePluginConfigRet | null => {
-	switch (packageName.toLowerCase()) {
-		case "unocss":
-			return ["UnoCss", "unocss/vite", true, {}];
-		case "vitepwa":
-			return ["VitePWA", "vite-plugin-pwa", false, {}];
-		case "solid-devtools":
-			return ["devtools", "solid-devtools/vite", true, {}];
-		default:
-			return null;
-	}
-};
-async function main() {
-	console.clear();
-	p.intro(`${color.bgCyan(color.black(" Solid-CLI "))}`);
-	const depsToAdd = (await p.multiselect({
-		options: [
-			{
-				value: "UnoCSS",
-				hint: "Utility Class CSS library, similar to Tailwind",
-			},
-			{
-				value: "VitePWA",
-				hint: "Minimal config PWA utility",
-			},
-			{
-				value: "solid-devtools",
-				label: "Solid Devtools",
-				hint: "SolidJS Devtools",
-			},
-		],
-		message: "What would you like to add",
-		required: true,
-	})) as string[];
-	const configData = (await readFile("vite.config.ts")).toString();
-	// Windows path hack. Not sure if this'll work on other platforms
-	const wasm_path = new URL(
-		"transform_config.wasm",
-		import.meta.url
-	).pathname.slice(1);
-	const extra_plugins = depsToAdd.map((dep) => resolvePluginConfig(dep));
-	const res = await transform(configData, {
-		filename: "vite.config.ts",
-		jsc: {
-			parser: {
-				syntax: "typescript",
-				tsx: false,
-			},
-			target: "es2022",
-			experimental: {
-				plugins: [
-					[
-						wasm_path,
-						{
-							additional_plugins: extra_plugins,
-						},
-					],
-				],
-			},
-		},
-	});
-	await writeFile("vite.config.ts", res.code);
-	p.log.success("Updated config");
-	p.log.info("Detecting package manager");
-	const manager = await detect();
-	const s = p.spinner();
-	s.start(`Installing plugins via ${manager}`);
-	for (const plugin of extra_plugins) {
-		if (!plugin) continue;
-		const res = await new Promise((res) =>
-			exec(`${manager} i ${plugin[1].toLowerCase().split("/")[0]}`, res)
-		);
-	}
-	s.stop("Installed plugins");
-}
+import { exec } from "child_process";
 
-main().catch(console.error);
+const transformer = new ConfigTransform();
+const add = command({
+	name: "add",
+	description: "Can add and install integration: `solid add unocss`",
+	args: {
+		package_name: positional({
+			type: oneOf(supported),
+			displayName: "Package Name",
+		}),
+	},
+	handler: async ({ package_name }) => {
+		const config = resolvePluginConfig(package_name);
+		if (!config) {
+			p.log.error(
+				"Couldn't resolve your desired package. Maybe we don't support it"
+			);
+			return;
+		}
+		await transformer.add_plugins([config]);
+		p.log.success("Config updated");
+		const pM = await detect();
+		const s = p.spinner();
+		s.start(`Installing packages via ${pM}`);
+		await new Promise((res) =>
+			exec(`${pM} i ${config[1].toLowerCase().split("/")[0]}`, res)
+		);
+		s.stop("Packages installed");
+	},
+});
+const cli = subcommands({
+	name: "solid",
+	cmds: { add },
+});
+console.clear();
+p.intro(`${color.bgCyan(color.black(" Solid-CLI "))}`);
+run(cli, process.argv.slice(2));
