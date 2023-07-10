8304bd993b8edca06524d36e21d58b6c4175c812
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-07-10 13:17:26 +0100

    Cleaned up code (switched approach to replacing value in vector, rather than iteratively adding the values)

--- swc_plugin/src/lib.rs
+++ swc_plugin/src/lib.rs
@@ -99,24 +99,17 @@ fn add_new_plugins(visitor: &mut TransformVisitor, arr_lit: &ArrayLit) -> PropOr
 }
 fn update_argument(visitor: &mut TransformVisitor, arg: &mut ExprOrSpread) {
     if let Expr::Object(obj) = arg.expr.as_expr() {
-        let mut new_props: Vec<PropOrSpread> = vec![];
+        let mut new_props: Vec<PropOrSpread> = obj.props.clone();
         let mut mutated_existing = false;
-        for prop_spread in obj.props.clone() {
+        for n in 0..new_props.len() {
+            let prop_spread = &new_props[n];
             if let PropOrSpread::Prop(prop) = &prop_spread {
                 if let Prop::KeyValue(key_value_prop) = &**prop && let Some(i) = key_value_prop.key.as_ident() {
                     if i.sym.to_string() == "plugins" && let Expr::Array(arr_lit) = &*key_value_prop.value {
-                        new_props.push(add_new_plugins(visitor, arr_lit));
+                        new_props[n] = add_new_plugins(visitor, arr_lit);
                         mutated_existing = true;
                     }
-                    else{
-                        new_props.push(prop_spread);
-                    }
                 }
-                else{
-                    new_props.push(prop_spread);
-                }
-            } else {
-                new_props.push(prop_spread);
             }
         }
         if !mutated_existing {
