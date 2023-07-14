2dff0ca028c1df0e9250988a2ed241a4c8255fe4
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-07-14 17:52:37 +0100

    Added more functions to `file_ops`

--- frontend/src/lib/utils/file_ops.ts
+++ frontend/src/lib/utils/file_ops.ts
@@ -1,6 +1,35 @@
 import { readFile, writeFile } from "fs/promises";
-
+export const readFileToString = async (path: string) => {
+	return (await readFile(path)).toString();
+};
 export const insertAtBeginning = async (filePath: string, text: string) => {
-	const contents = (await readFile(filePath)).toString();
+	const contents = await readFileToString(filePath);
 	await writeFile(filePath, text + contents);
+};
+export const insertAtEnd = async (filePath: string, text: string) => {
+	const contents = await readFileToString(filePath);
+	await writeFile(filePath, contents + text);
+};
+export const replaceString = async (
+	filePath: string,
+	search: string | RegExp,
+	replace: string
+) => {
+	const contents = await readFileToString(filePath);
+	contents.replace(search, replace);
+	await writeFile(filePath, contents);
+};
+export const insertBefore = async (
+	filePath: string,
+	search: string | RegExp,
+	text: string
+) => {
+	await replaceString(filePath, search, text + search);
+};
+export const insertAfter = async (
+	filePath: string,
+	search: string | RegExp,
+	text: string
+) => {
+	await replaceString(filePath, search, search + text);
 };
