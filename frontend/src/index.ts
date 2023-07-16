18325b11e77016d515c6f05b54471eadc1470e55
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-07-16 20:19:36 +0100

    Scaffolded CLI "wizard", which runs when the user provides no args

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -3,9 +3,39 @@ import { run, subcommands } from "cmd-ts";
 import * as p from "@clack/prompts";
 import color from "picocolors";
 import commands from "./plugins/plugins_entry";
-const cli = subcommands({
-  name: "solid",
-  cmds: commands,
-});
-p.intro(`${color.bgCyan(color.black(" Solid-CLI "))}`);
-run(cli, process.argv.slice(2));
+const possibleActions = [
+  { value: "add", label: "Add an integration", hint: "solid add ..." },
+  { value: "new", label: "Create new project", hint: "solid new ..." },
+  { value: "start", label: "A start specific action", hint: "solid start ..." },
+] as const;
+const provideSuggestions = async () => {
+  type ActionType = (typeof possibleActions)[number]["value"];
+  let action = (await p.select({
+    message: "What would you like to do?",
+    // This thing really doesn't like `as const` things
+    options: possibleActions as any,
+  })) as ActionType;
+  if (!action) return;
+  switch (action) {
+    case "add":
+      // Launch add autocomplete
+      break;
+    case "new":
+      // Launch new autocomplete
+      break;
+    case "start":
+      // Launch start autocomplete
+      break;
+  }
+};
+const main = async () => {
+  const cli = subcommands({
+    name: "solid",
+    cmds: commands,
+  });
+  p.intro(`${color.bgCyan(color.black(" Solid-CLI "))}`);
+  const args = process.argv.slice(2);
+  if (args.length === 0) await provideSuggestions();
+  run(cli, args);
+};
+main();
