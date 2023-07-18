e6221967eb98a2bb705423abacaebd6bea5de94c
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-07-18 20:44:07 +0100

    Added reactivity functions

---
+++ packages/core/src/reactivity/core.ts
@@ -0,0 +1,21 @@
+let OBSERVER: (() => any) | null = null;
+
+export const createSignal = <T>(val: T) => {
+  const listeners: Set<() => any> = new Set();
+  const get = () => {
+    if (OBSERVER) listeners.add(OBSERVER);
+    return val;
+  };
+  const set = (newVal: T) => {
+    val = newVal;
+    listeners.forEach((listener) => listener());
+  };
+  return [get, set] as [() => T, (val: T) => void];
+};
+
+export const createEffect = <T>(fn: () => T) => {
+  const prev = fn;
+  OBSERVER = fn;
+  fn();
+  OBSERVER = prev;
+};
