8aee8f20cf6af6b22f56da11e5b534c97f663975
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-07-18 19:44:20 +0100

    Changed default name

--- packages/core/src/lib/start/add_route.ts
+++ packages/core/src/lib/start/add_route.ts
@@ -7,6 +7,6 @@ export const createRoute = async (path: string, name?: string) => {
   const path_parts = path.split("/");
   path_parts.unshift("src", "routes");
   await mkdir(path_parts.join("/"), { recursive: true });
-  path_parts.push(`${name}.tsx` ?? "index.tsx");
+  path_parts.push(`${name}.tsx` ?? "Index.tsx");
   await writeFile(path_parts.join("/"), default_file);
 };
