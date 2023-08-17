c510434010925e8be92360706308e065a0a25c22
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2023-08-17 13:51:35 +0100

    Add new CLI which solely deals with creating new projects Bump versions

---
+++ packages/create-solid/src/index.ts
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+import { handleNew } from "@solid-cli/commands";
+const main = async () => {
+	await handleNew();
+};
+main();
---
+++ packages/create-solid/tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+import metaUrlPlugin from "@chialab/esbuild-plugin-meta-url";
+
+export default defineConfig({
+	entry: ["src/index.ts"],
+	target: "esnext",
+	format: "esm",
+	splitting: false,
+	sourcemap: true,
+	clean: true,
+	esbuildPlugins: [metaUrlPlugin()],
+});
