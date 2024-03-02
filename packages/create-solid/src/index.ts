f4c0f42f7ca24326992c3202b8ac1ba911ea0e36
Dario Piotrowicz <dario@cloudflare.com>
2024-03-02 18:14:49 +0000

    allow users to provide some cli arguments to create-solid
    add the possibility for users to provide cli arguments to the create-solid
    command streamlining common use cases
    
    the added arguements are:
     - `-p | --project-name` to specify the name of the project
     - `-s | --solid-start` to request the creation of a solid-start project

--- packages/create-solid/src/index.ts
+++ packages/create-solid/src/index.ts
@@ -3,5 +3,23 @@ import { handleNew } from "@solid-cli/commands/new";
 import color from "picocolors";
 import { intro } from "@clack/prompts";
 import { version } from "../package.json";
+import { program } from "commander";
+
 intro(`\n${color.bgCyan(color.black(` Create-Solid v${version}`))}`);
-handleNew();
+
+program
+	.allowExcessArguments(false)
+	.configureHelp({
+		commandUsage: () => "create-solid [options]",
+	})
+	.helpOption(undefined, "Shows this help message")
+	.option("-p, --project-name <VALUE>", "The name of your project")
+	.option("-s, --solid-start", "Create a SolidStart project");
+
+program.parse();
+const { projectName, solidStart } = program.opts<{
+	projectName?: string;
+	solidStart?: boolean;
+}>();
+
+handleNew(undefined, projectName, false, solidStart);
