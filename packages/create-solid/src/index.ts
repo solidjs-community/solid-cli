3b5f536b1721f8bb587f23558ab614a8bb1874c7
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-03-04 18:15:23 +0000

    Move to `cmd-ts` for argument parsing

--- packages/create-solid/src/index.ts
+++ packages/create-solid/src/index.ts
@@ -3,24 +3,24 @@ import { handleNew } from "@solid-cli/commands/new";
 import color from "picocolors";
 import { intro } from "@clack/prompts";
 import { version } from "../package.json";
-import { program } from "commander";
-import { t } from "@solid-cli/utils";
+import { command, run, option, string, boolean, flag, optional } from "cmd-ts";
 
 intro(`\n${color.bgCyan(color.black(` Create-Solid v${version}`))}`);
 
-program
-	.allowExcessArguments(false)
-	.configureHelp({
-		commandUsage: () => `create-solid [${t.OPTIONS}]`,
-	})
-	.helpOption(undefined, t.SHOWS_THIS_HELP_MESSAGE)
-	.option(`-p, --project-name <${t.VALUE}>`, t.NAME_OF_YOUR_PROJECT)
-	.option("-s, --solid-start", t.CREATE_START_PROJECT);
-
-program.parse();
-const { projectName, solidStart } = program.opts<{
-	projectName?: string;
-	solidStart?: boolean;
-}>();
-
-handleNew(undefined, projectName, false, solidStart);
+const app = command({
+	name: "create-solid",
+	description: "Create a new Solid project",
+	args: {
+		projectName: option({ type: optional(string), long: "project-name", short: "p", description: "Name of your project" }),
+		solidStart: flag({
+			type: optional(boolean),
+			long: "solid-start",
+			short: "s",
+			description: "Create a SolidStart project",
+		}),
+	},
+	handler: async ({ projectName, solidStart }) => {
+		await handleNew(undefined, projectName, false, solidStart);
+	},
+});
+run(app, process.argv.slice(2));
