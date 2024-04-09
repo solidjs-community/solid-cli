3c68d388d55691b2a53056fcb1d27f52c5383164
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2024-04-09 21:20:06 +0100

    feat: add project name as positional argument, closes #40

--- packages/create-solid/src/index.ts
+++ packages/create-solid/src/index.ts
@@ -3,7 +3,7 @@ import { handleNew } from "@solid-cli/commands/new";
 import color from "picocolors";
 import { intro } from "@clack/prompts";
 import { version } from "../package.json";
-import { command, run, option, string, boolean, flag, optional } from "cmd-ts";
+import { command, run, option, string, boolean, flag, optional, positional } from "cmd-ts";
 
 intro(`\n${color.bgCyan(color.black(` Create-Solid v${version}`))}`);
 
@@ -11,7 +11,8 @@ const app = command({
 	name: "create-solid",
 	description: "Create a new Solid project",
 	args: {
-		projectName: option({ type: optional(string), long: "project-name", short: "p", description: "Name of your project" }),
+		projectNamePositional: positional({type: optional(string), displayName: "Project Name", description: "The name of the project to be generated"}),
+		projectNameOption: option({ type: optional(string), long: "project-name", short: "p", description: "Name of your project" }),
 		solidStart: flag({
 			type: optional(boolean),
 			long: "solid-start",
@@ -19,8 +20,8 @@ const app = command({
 			description: "Create a SolidStart project",
 		}),
 	},
-	handler: async ({ projectName, solidStart }) => {
-		await handleNew(undefined, projectName, false, solidStart);
+	handler: async ({ projectNameOption, solidStart, projectNamePositional }) => {
+		await handleNew(undefined, projectNamePositional ?? projectNameOption, false, solidStart);
 	},
 });
 run(app, process.argv.slice(2));
