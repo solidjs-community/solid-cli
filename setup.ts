d066fbe6a0d13914cc6e6ea79440d76f7fbcf4af
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-01-19 14:28:38 +0000

    fix: only remove `test` directory if it exists

--- setup.ts
+++ setup.ts
@@ -1,6 +1,7 @@
 import type { TestProject } from 'vitest/node'
-import { rmSync } from "fs";
+import { existsSync, rmSync } from "fs";
 export default function setup(project: TestProject) {
     //   Clean up test directory before running tests
-    rmSync("./test", { recursive: true })
+    if (existsSync("./test"))
+        rmSync("./test", { recursive: true })
 }
