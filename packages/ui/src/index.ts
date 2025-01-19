6477e9909a46f56e02994dd99d2d66482cf71c45
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-01-19 00:48:46 +0000

    remove unnecessary utils
    remove unused `ui` package

--- packages/ui/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./components/autocomplete";
-export * from "./components/form";
-export * from "./components/autocomplete/utils";
-export * from "./components/spinnerify";
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,2 @@
-export * from "./translations";
-export * from "./config";
 export * from "./start";
 export * from "./open";
-export * from "./cmd-ts";
