9294c1b905a0e7f7c8d7832318ee70cf141847b6
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-01-22 21:55:34 +0000

    feat: begin adding command for pretty printing useful debug info

---
+++ packages/full-solid/src/debug/index.ts
@@ -0,0 +1,45 @@
+/// This subcommand prints useful debug info to stdout
+import { readFile } from "fs/promises";
+import os from "os";
+type DebugInfo = {
+	runtime: { name: string, version: string }
+	os: { platform: string, release: string }
+	packages: Record<string, string>
+}
+const getPackageJSON = async () => {
+	const f = await readFile("package.json");
+	const parsed = JSON.parse(f.toString());
+	return parsed;
+}
+const prettyPrintRecord = (record: Record<string, string>) => {
+	let str = "";
+	for (const key in record) {
+		const value = record[key];
+		str += `  ${key}: ${value}\n`
+	}
+	return str;
+}
+
+export const prettyPrint = (info: DebugInfo) => {
+	return `System:
+  OS: ${info.os.platform} ${info.os.release}
+Runtime:
+  ${info.runtime.name}: v${info.runtime.version}
+${Object.keys(info.packages).length !== 0 ? `Dependencies:
+${prettyPrintRecord(info.packages)}` : ""}`
+}
+export const fetchDebugInfo = async (): Promise<DebugInfo> => {
+	const parsed = await getPackageJSON();
+	const packages: Record<string, string> = parsed.dependencies ?? {};
+	return {
+		runtime: {
+			name: "Node",
+			version: "22.10.4"
+		},
+		os: {
+			platform: os.platform(),
+			release: os.release()
+		},
+		packages
+	}
+}
---
+++ packages/full-solid/tests/debuginfo.test.ts
@@ -0,0 +1,6 @@
+import { it } from "vitest";
+import { fetchDebugInfo, prettyPrint } from "../src/debug";
+
+it("Runs", async () => {
+  console.log(prettyPrint(await fetchDebugInfo()))
+})
