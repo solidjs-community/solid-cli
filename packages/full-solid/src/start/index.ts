2cfc25b6a17289278cebc0b58df55d7a2a97a6c0
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-01-23 19:47:13 +0000

    feat: add colour to `route` subcommand output

--- packages/full-solid/src/start/index.ts
+++ packages/full-solid/src/start/index.ts
@@ -1,6 +1,7 @@
 import { createRoute } from "@solid-cli/utils";
 import { defineCommand } from "citty";
 import * as p from "@clack/prompts";
+import { green } from "picocolors";
 export const startCommands = defineCommand({
 	meta: { description: "Start-specific commands" }, subCommands: {
 		route: defineCommand({
@@ -13,7 +14,7 @@ export const startCommands = defineCommand({
 			},
 			async run({ args: { path } }) {
 				await createRoute(path as string);
-				p.log.success(`Route ${path} successfully created!`)
+				p.log.success(`Route ${green(path as string)} successfully created!`)
 			},
 		})
 	}
