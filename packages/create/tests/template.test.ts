11215c0fbbe0b620f27f55b48d30b23c37d18c33
Thomas Beer <71586988+Tommypop2@users.noreply.github.com>
2025-09-19 19:59:30 +0100

    test: fix test for downloading templates

--- packages/create/tests/template.test.ts
+++ packages/create/tests/template.test.ts
@@ -1,8 +1,8 @@
 import { expect, it } from "vitest";
 import { createVanilla } from "../src";
 import { existsSync } from "fs";
-it("downloads and extracts the typescript template", async () => {
-	await createVanilla({ template: "ts", destination: "./test/ts" }, false);
+it("downloads and extracts the basic template", async () => {
+	await createVanilla({ template: "basic", destination: "./test/ts" }, false);
 
 	const appTsx = existsSync("./test/ts/src/App.tsx");
 	expect(appTsx).toBe(true);
